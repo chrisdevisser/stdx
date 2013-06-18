2078474f75f8da3ff4a3b06079fc9241a8b6f228
Chris DeVisser <chrisndevisser@gmail.com>
2013-06-18 02:58:19 -0400

    Mostly just some operators on Angle.

--- angle.hh
+++ angle.hh
@@ -18,6 +18,9 @@ struct Angle {
     friend class Degrees;
     friend class Radians;
 
+    constexpr Angle() noexcept
+        : Angle{0.} {}
+
     double deg() const noexcept {
         return rad_ * 180 / PI;
     }
@@ -26,15 +29,59 @@ struct Angle {
         return rad_;
     }
 
+    Angle &operator+=(const Angle &rhs) noexcept {
+        rad_ += rhs.rad_;
+        return *this;
+    }
+
+    Angle &operator-=(const Angle &rhs) noexcept {
+        rad_ -= rhs.rad_;
+        return *this;
+    }
+
+    Angle &operator*=(const double rhs) noexcept {
+        rad_ *= rhs;
+        return *this;
+    }
+
+    Angle &operator/=(const double rhs) noexcept {
+        rad_ /= rhs;
+        return *this;
+    }
+
 private:
     double rad_;
 
-    constexpr Angle(double r) noexcept
+    constexpr Angle(const double r) noexcept
         : rad_{r} {}
 };
 
+inline Angle operator+(Angle lhs, const Angle &rhs) noexcept {
+    return lhs += rhs;
+}
+
+inline Angle operator-(Angle lhs, const Angle &rhs) noexcept {
+    return lhs -= rhs;
+}
+
+inline Angle operator*(Angle lhs, const double rhs) noexcept {
+    return lhs *= rhs;
+}
+
+inline Angle operator*(const double lhs, Angle rhs) noexcept {
+    return rhs *= lhs;
+}
+
+inline Angle operator/(Angle lhs, const double rhs) noexcept {
+    return lhs /= rhs;
+}
+
+inline Angle operator/(const double lhs, Angle rhs) noexcept {
+    return rhs /= lhs;
+}
+
 struct Degrees {
-    constexpr explicit Degrees(double deg) noexcept
+    constexpr explicit Degrees(const double deg) noexcept
         : deg_{deg} {}
 
     operator Angle() const noexcept {
@@ -46,7 +93,7 @@ private:
 };
 
 struct Radians {
-    constexpr explicit Radians(double rad) noexcept
+    constexpr explicit Radians(const double rad) noexcept
         : rad_{rad} {}
 
     operator Angle() const noexcept {
@@ -57,20 +104,20 @@ private:
     double rad_;
 };
 
-inline constexpr Degrees operator"" _deg(long double d) noexcept {
-    return Degrees(d);
+inline constexpr Degrees operator"" _deg(const long double d) noexcept {
+    return Degrees{d};
 }
 
-inline constexpr Degrees operator"" _deg(unsigned long long d) noexcept {
-    return Degrees(d);
+inline constexpr Degrees operator"" _deg(const unsigned long long d) noexcept {
+    return Degrees{d};
 }
 
-inline constexpr Radians operator"" _rad(long double r) noexcept {
-    return Radians(r);
+inline constexpr Radians operator"" _rad(const long double r) noexcept {
+    return Radians{r};
 }
 
-inline constexpr Radians operator"" _rad(unsigned long long r) noexcept {
-    return Radians(r);
+inline constexpr Radians operator"" _rad(const unsigned long long r) noexcept {
+    return Radians{r};
 }
 
 }
